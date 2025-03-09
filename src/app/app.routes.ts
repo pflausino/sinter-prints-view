575d64701518bb8d06174e9bfc7d1c88acc01f6e
Paulo Flausino <paulo.flausino@iatec.com>
2025-03-09 17:24:51 -0300

    feature: create new search domain and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './domains/artwork/features/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo:'search', pathMatch:'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'create', component: AppComponent },
+  { path: 'not-found', component: AppComponent },
+  { path: '**', redirectTo:'not-found', pathMatch:'full'},
+];
