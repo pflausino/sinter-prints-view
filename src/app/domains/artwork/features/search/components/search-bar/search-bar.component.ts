e7b938c4dd0b56235309743a4bf93cea020a33a9
Paulo Flausino <paulo.flausino@iatec.com>
2025-03-09 18:22:09 -0300

    feature: import material ui components

--- src/app/domains/artwork/features/search/components/search-bar/search-bar.component.ts
+++ src/app/domains/artwork/features/search/components/search-bar/search-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
