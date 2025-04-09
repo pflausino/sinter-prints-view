3f80b1fed88a9cf01bb8f6752e8dac513b15f10a
Paulo Flausino <calderaro95@hotmail.com>
2025-04-09 06:53:30 -0300

    feat: some global Style
    AI Driven (Cursor)

--- src/app/domains/artwork/features/create/components/create-form/create-form.component.ts
+++ src/app/domains/artwork/features/create/components/create-form/create-form.component.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-form',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './create-form.component.html',
   styleUrl: './create-form.component.scss'
 })
